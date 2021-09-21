3f5fbf7f9f1d32131a713cf4cb92183f59bc5b3d
sonokr <ryotakusunoki@hotmail.com>
2021-09-21 23:25:32 +0900

    added comments. adjusted the layout and appearance.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
 import React, { useEffect } from "react";
 import { useKey } from "rooks";
 import "./App.css";
-import { Row, Col, Input, Button, List } from "antd";
+import { Row, Col, Input, Button, List, Radio, Typography } from "antd";
 import InfiniteScroll from "react-infinite-scroller";
 import { CaretLeftOutlined, CaretRightOutlined } from "@ant-design/icons";
+const { Text } = Typography;
 const { useState, useRef } = React;
 
 interface Annotation {
+  id: number;
+  filename: string;
   x: number;
   y: number;
   visibility: number;
+  status: number;
 }
 
 interface AnnotationList {
   [key: string]: Annotation;
+}
+
+interface Dict {
+  [key: number | string | symbol]: any;
 }
 
 const getExt = (filename: string) => {
@@ -25,8 +33,9 @@ const getExt = (filename: string) => {
 const App = () => {
   const initValue = -1;
 
+  // const [isDirectorySelected, setIsDirectorySelected] = useState(false);
   const [fileList, setFileList] = useState<File[]>([]);
-  const [numberOfImage, setNumberOfImage] = useState(0);
+  const [numberOfImage, setNumberOfImage] = useState(-1);
   const [index, setIndex] = useState(0);
   const [imageDetail, setImageDetail] = useState({
     filename: "",
@@ -34,49 +43,99 @@ const App = () => {
     height: 0,
   });
   const [annotation, setAnnotation] = useState<Annotation>({
+    id: 0,
+    filename: "",
     x: initValue,
     y: initValue,
     visibility: initValue,
+    status: initValue,
   });
   const [annotationList, setAnnotationList] = useState<AnnotationList>({});
+  const [isLabeledImageList, setIsLabeledImageList] = useState<Boolean[]>([]);
 
   let isDrawing = false;
 
+  // Canvas
+  const canvasWrapperRef = useRef<HTMLDivElement>(null);
+  const canvasWrapper = canvasWrapperRef.current;
   const imageCanvasRef = useRef<HTMLCanvasElement>(null);
+  const imageCanvas = imageCanvasRef.current;
+  const imageContext = imageCanvas?.getContext("2d");
   const annotationCanvasRef = useRef<HTMLCanvasElement>(null);
+  const annotationCanvas = annotationCanvasRef.current;
+  const annotationContext = annotationCanvas?.getContext("2d");
 
+  // Resize the annotation canvas as the image is resize.
   useEffect(() => {
-    if (!annotationCanvasRef.current) return;
-    annotationCanvasRef.current.width = imageDetail.width;
-    annotationCanvasRef.current.height = imageDetail.height;
+    if (!annotationCanvas) return;
+    annotationCanvas.width = imageDetail.width;
+    annotationCanvas.height = imageDetail.height;
+    // eslint-disable-next-line
   }, [imageDetail.width, imageDetail.height]);
 
+  // Once a directory has been selected,
+  // craete a list of the same size for annotation.
   useEffect(() => {
     const newAnnotationList: AnnotationList = {};
-    for (const file of fileList) {
+    const newIsLabeledList: Dict = {};
+    for (const [index, file] of fileList.entries()) {
       newAnnotationList[file.name] = {
+        id: index,
+        filename: file.name,
         x: initValue,
         y: initValue,
         visibility: initValue,
+        status: initValue,
       };
+      newIsLabeledList[file.name] = false;
     }
     setAnnotationList(newAnnotationList);
+    setIsLabeledImageList(Array(fileList.length).fill(false));
     // eslint-disable-next-line
   }, [fileList]);
 
+  // Records an index of the images that have been labeled.
+  useEffect(() => {
+    if (
+      annotation.x === initValue ||
+      annotation.y === initValue ||
+      annotation.visibility === initValue ||
+      annotation.status === initValue
+    )
+      return;
+
+    const temp = [];
+    for (const [index, value] of isLabeledImageList.entries()) {
+      if (annotation.id === index) temp.push(true);
+      else temp.push(value);
+    }
+    setIsLabeledImageList(temp);
+    // eslint-disable-next-line
+  }, [
+    index,
+    annotation.x,
+    annotation.y,
+    annotation.visibility,
+    annotation.status,
+  ]);
+
+  // When the index is changed, the image is redrawn and,
+  // annotation information is update.
   useEffect(() => {
     if (fileList.length === 0) return;
     const file = fileList[index];
 
+    if (!imageCanvas || !canvasWrapper) return;
+    imageCanvas.width = canvasWrapper.clientWidth;
+    imageCanvas.height = canvasWrapper.clientHeight;
+
     const img = new Image();
     img.src = window.URL.createObjectURL(file);
     img.onload = () => {
-      if (!imageCanvasRef.current) return;
-      imageCanvasRef.current.width = img.width;
-      imageCanvasRef.current.height = img.height;
-      const context = imageCanvasRef.current.getContext("2d");
-      if (!context) return;
-      context.drawImage(img, 0, 0);
+      if (!imageCanvas || !imageContext) return;
+      const scale = imageCanvas.width / img.width;
+      imageContext.setTransform(scale, 0, 0, scale, 0, 0);
+      imageContext.drawImage(img, 0, 0);
 
       setImageDetail({
         filename: file.name,
@@ -89,9 +148,12 @@ const App = () => {
     if (annotationList[file.name] !== undefined) {
       const currentAnnotation = annotationList[file.name];
       setAnnotation({
+        id: currentAnnotation.id,
+        filename: currentAnnotation.filename,
         x: currentAnnotation.x,
         y: currentAnnotation.y,
         visibility: currentAnnotation.visibility,
+        status: currentAnnotation.status,
       });
       drawCircle(currentAnnotation.x, currentAnnotation.y);
     }
@@ -111,14 +173,37 @@ const App = () => {
       .sort(compare);
     setFileList(newFileList);
     setNumberOfImage(newFileList.length - 1);
+    console.log(newFileList);
   };
 
   const nextImage = (e: any) => {
+    setAnnotationList({
+      ...annotationList,
+      [imageDetail.filename]: {
+        id: annotation.id,
+        filename: annotation.filename,
+        x: annotation.x,
+        y: annotation.y,
+        visibility: annotation.visibility,
+        status: annotation.status,
+      },
+    });
     if (index === numberOfImage) setIndex(0);
     else setIndex(index + 1);
   };
 
   const previousImage = (e: any) => {
+    setAnnotationList({
+      ...annotationList,
+      [imageDetail.filename]: {
+        id: annotation.id,
+        filename: annotation.filename,
+        x: annotation.x,
+        y: annotation.y,
+        visibility: annotation.visibility,
+        status: annotation.status,
+      },
+    });
     if (index === 0) setIndex(numberOfImage);
     else setIndex(index - 1);
   };
@@ -127,18 +212,16 @@ const App = () => {
   useKey("ArrowLeft", previousImage);
 
   const drawCircle = (x: number, y: number) => {
-    if (!annotationCanvasRef.current) return;
-    const context = annotationCanvasRef.current?.getContext("2d");
-    if (!context) return;
-    context.clearRect(
+    if (!annotationCanvas || !annotationContext) return;
+    annotationContext.clearRect(
       0,
       0,
-      annotationCanvasRef.current.width,
-      annotationCanvasRef.current.height
+      annotationCanvas.width,
+      annotationCanvas.height
     );
     if (x === initValue && y === initValue) return;
-    context.beginPath();
-    context.arc(
+    annotationContext.beginPath();
+    annotationContext.arc(
       x, // Coordinates of the x-centre of the circle
       y, // Coordinates of the y-center of the circle
       5, // Radius
@@ -146,8 +229,8 @@ const App = () => {
       (360 * Math.PI) / 180, // Ending angle
       false // Direction
     );
-    context.fillStyle = "rgba(255,0,0,1.0)";
-    context.fill();
+    annotationContext.fillStyle = "rgba(255,0,0,1.0)";
+    annotationContext.fill();
   };
 
   const handleMouseDown = (x: number, y: number) => {
@@ -163,19 +246,13 @@ const App = () => {
   const handleMouseUp = (x: number, y: number) => {
     isDrawing = false;
     drawCircle(x, y);
-    setAnnotation({ x: x, y: y, visibility: annotation.visibility });
-    setAnnotationList({
-      ...annotationList,
-      [imageDetail.filename]: {
-        x: x,
-        y: y,
-        visibility: 0,
-      },
+    setAnnotation({
+      ...annotation,
+      x: x,
+      y: y,
+      visibility: annotation.visibility,
+      status: annotation.status,
     });
-  };
-
-  const onVisibilityChange = (visibility: number) => {
-    return;
   };
 
   const outputLabel = () => {
@@ -191,10 +268,6 @@ const App = () => {
     link.download = "Label.csv";
     link.href = URL.createObjectURL(blob);
     link.click();
-  };
-
-  const handleInfiniteOnLoad = () => {
-    return;
   };
 
   return (
@@ -214,50 +287,82 @@ const App = () => {
             webkitdirectory=""
           />
           <table>
-            <tr>
-              <th>Name</th>
-              <td>
-                <Input disabled value={imageDetail.filename} id="filename" />
-              </td>
-            </tr>
-            <tr>
-              <th>Width</th>
-              <td>
-                <Input disabled value={imageDetail.width} id="width" />
-              </td>
-            </tr>
-            <tr>
-              <th>Height</th>
-              <td>
-                <Input disabled value={imageDetail.height} id="height" />
-              </td>
-            </tr>
-            <tr>
-              <th>X coordinate</th>
-              <td>
-                <Input disabled value={annotation.x} id="xCoordinate" />
-              </td>
-            </tr>
-            <tr>
-              <th>Y coordinate</th>
-              <td>
-                <Input disabled value={annotation.y} id="yCoordinate" />
-              </td>
-            </tr>
-            <tr>
-              <th>Visibility</th>
-              <td>
-                <Input
-                  onChange={(e) => onVisibilityChange(Number(e.target.value))}
-                  value={annotation.visibility}
-                  type="number"
-                  id="visibility"
-                />
-              </td>
-            </tr>
+            <tbody>
+              <tr>
+                <th>Name</th>
+                <td>
+                  <Input disabled value={imageDetail.filename} id="filename" />
+                </td>
+              </tr>
+              <tr>
+                <th>Width</th>
+                <td>
+                  <Input disabled value={imageDetail.width} id="width" />
+                </td>
+              </tr>
+              <tr>
+                <th>Height</th>
+                <td>
+                  <Input disabled value={imageDetail.height} id="height" />
+                </td>
+              </tr>
+              <tr>
+                <th>X coordinate</th>
+                <td>
+                  <Input disabled value={annotation.x} id="xCoordinate" />
+                </td>
+              </tr>
+              <tr>
+                <th>Y coordinate</th>
+                <td>
+                  <Input disabled value={annotation.y} id="yCoordinate" />
+                </td>
+              </tr>
+              <tr>
+                <th>Visibility</th>
+                <td>
+                  <Radio.Group
+                    options={[
+                      { label: "0", value: 0 },
+                      { label: "1", value: 1 },
+                      { label: "2", value: 2 },
+                      { label: "3", value: 3 },
+                    ]}
+                    onChange={(e) =>
+                      setAnnotation({
+                        ...annotation,
+                        visibility: Number(e.target.value),
+                      })
+                    }
+                    value={annotation.visibility}
+                    optionType="button"
+                  />
+                </td>
+              </tr>
+              <tr>
+                <th>Status</th>
+                <td>
+                  <Radio.Group
+                    options={[
+                      { label: "0", value: 0 },
+                      { label: "1", value: 1 },
+                      { label: "2", value: 2 },
+                    ]}
+                    onChange={(e) =>
+                      setAnnotation({
+                        ...annotation,
+                        status: Number(e.target.value),
+                      })
+                    }
+                    value={annotation.status}
+                    optionType="button"
+                  />
+                </td>
+              </tr>
+            </tbody>
           </table>
           <p>
-            {index}/{numberOfImage}
+            {index + 1}/{numberOfImage + 1}
           </p>
           <div>
             <Button
@@ -274,35 +379,57 @@ const App = () => {
           {/* TODO:
               When each element in the list is clicked,
               the image should be displayed. */}
-          <div style={{ height: "700px", overflow: "auto" }}>
+          <div style={{ height: "300px", overflow: "auto" }}>
             <InfiniteScroll
               initialLoad={false}
               pageStart={0}
-              loadMore={handleInfiniteOnLoad}
+              loadMore={() => {
+                return;
+              }}
               hasMore={false}
               useWindow={false}
             >
               <List
                 dataSource={fileList}
-                footer={
-                  <div>
-                    <b>Labeded images: 0/100</b>
-                  </div>
-                }
-                renderItem={(item) => (
-                  <List.Item key={item.name}>
+                renderItem={(item, index) => (
+                  <List.Item key={index} onClick={() => setIndex(index)}>
                     <div>{item.name}</div>
-                    <div>Not Labeled</div>
+                    <div>
+                      {isLabeledImageList[index] === true ? (
+                        <Text type="success">Labeled</Text>
+                      ) : (
+                        <Text type="danger">Not Labeled</Text>
+                      )}
+                    </div>
                   </List.Item>
                 )}
               ></List>
             </InfiniteScroll>
           </div>
+          <div>
+            <div>
+              <b>
+                All images are{" "}
+                {isLabeledImageList.filter((a) => a === true).length - 1 !==
+                numberOfImage ? (
+                  <Text type="danger">NOT </Text>
+                ) : (
+                  ""
+                )}
+                labeled.
+              </b>
+            </div>
+            <Text>
+              Labeded images:{" "}
+              {isLabeledImageList.filter((a) => a === true).length}/
+              {numberOfImage + 1}
+            </Text>
+          </div>
           <Button onClick={outputLabel}>Output Label.csv</Button>
         </Col>
         <Col>
           <div>
-            <div className="canvas-wrapper">
+            <div ref={canvasWrapperRef} className="canvas-wrapper">
               <canvas ref={imageCanvasRef} id="image"></canvas>
               <canvas
                 ref={annotationCanvasRef}
